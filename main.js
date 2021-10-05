235f527ead09b5185742f4a860e3a5655fd29a3a
Szalai Szabolcs <sz.szalai16@gmail.com>
2021-10-05 18:45:54 +0200

    hozzáadásnál egyből kilistázza, a táblázat rendesen működik

--- main.js
+++ main.js
@@ -37,50 +37,26 @@ function hozzaad() {
     document.getElementById('filmNev').value = "";
     document.getElementById('filmRendezo').value = "";
     document.getElementById('filmEv').value = "";
-}
 
-function listaz() {
-    var writeTo = document.getElementById('tabla')
-    var table = document.createElement('table');
+    var tablazat = document.getElementById('tabla');
+    var tableBody = document.getElementById('tablaAdatok');
 
-    var tr = document.createElement('tr');
+    tablazat.style.display = "table";
 
-    var th1 = document.createElement('th');
-    var th2 = document.createElement('th');
-    var th3 = document.createElement('th');
+    tableBody.innerHTML = "";
 
-    var text1 = document.createTextNode("Film címe");
-    var text2 = document.createTextNode("Rendező neve");
-    var text3 = document.createTextNode("Film kiadási éve");
-
-    th1.appendChild(text1);
-    th2.appendChild(text2);
-    th3.appendChild(text3);
-    tr.appendChild(th1);
-    tr.appendChild(th2);
-    tr.appendChild(th3);
-
-    table.appendChild(tr);
-    
-    for (var i = 0; i < adatok.length; i++){
-        var tr = document.createElement('tr');   
-    
-        var td1 = document.createElement('td');
-        var td2 = document.createElement('td');
-        var td3 = document.createElement('td');
-
-        var text1 = document.createTextNode(adatok[i].getNev());
-        var text2 = document.createTextNode(adatok[i].getRendezo());
-        var text3 = document.createTextNode(adatok[i].getEv());
-
+    for (let i = 0; i < adatok.length; i++) {
+        let row = tableBody.insertRow();
+        let td1 = row.insertCell();
+        let text1 = document.createTextNode(adatok[i].getNev());
         td1.appendChild(text1);
+
+        let td2 = row.insertCell();
+        let text2 = document.createTextNode(adatok[i].getRendezo());
         td2.appendChild(text2);
+
+        let td3 = row.insertCell();
+        let text3 = document.createTextNode(adatok[i].getEv());
         td3.appendChild(text3);
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        tr.appendChild(td3);
-    
-        table.appendChild(tr);
     }
-    writeTo.appendChild(table);
 }
